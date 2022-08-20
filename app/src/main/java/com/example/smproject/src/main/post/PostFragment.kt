d321688f6f5b0db34a1a15fde8603ba4c6b0e691
Larry <yosan7939@gmail.com>
2022-08-20 23:52:25 +0900

    220820

--- app/src/main/java/com/example/smproject/src/main/post/PostFragment.kt
+++ app/src/main/java/com/example/smproject/src/main/post/PostFragment.kt
@@ -3,17 +3,18 @@ package com.example.smproject.src.main.post
 import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.CompoundButton
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.appcompat.widget.SwitchCompat
 import com.example.smproject.R
 import com.example.smproject.config.BaseFragment
-import com.example.smproject.databinding.FragmentInfoBinding
 import com.example.smproject.databinding.FragmentPostBinding
-import com.example.smproject.src.main.info.InfoFragment
+
 
 class PostFragment : BaseFragment<FragmentPostBinding>(FragmentPostBinding::bind,R.layout.fragment_post) {
-
+    private lateinit var swShare:SwitchCompat
+    private lateinit var tvShare:AppCompatTextView
     fun newInstance(): Fragment {
         return PostFragment()
     }
@@ -23,5 +24,17 @@ class PostFragment : BaseFragment<FragmentPostBinding>(FragmentPostBinding::bind
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        swShare = binding.postSwitch
+        tvShare = binding.postSwitchTv
+
+        swShare.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                //체크된 상태로 만들 시 코드
+                tvShare.text = "전체 공개"
+            } else {
+                //체크된 상태 취소 시 코드
+                tvShare.text = "비공개"
+            }
+        }
     }
 }
