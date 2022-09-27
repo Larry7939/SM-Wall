fa2d6b93d2e946e566854c95263c4a07b9f0a2ff
{ETKIM0414} <{etkim0414}>
2022-09-27 09:43:18 +0900

    220927

--- app/src/main/java/com/example/smproject/src/main/adapter/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/smproject/src/main/adapter/RecyclerViewAdapter.kt
@@ -1,24 +1,38 @@
 package com.example.smproject.src.main.adapter
 
+import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.annotation.NonNull
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.smproject.R
 import com.example.smproject.databinding.ItemListBinding
-import com.example.smproject.src.main.getPostApi.GetPostListService
-import com.example.smproject.src.main.getPostApi.models.GetPostListRequest
+import com.example.smproject.databinding.PostListBinding
+import com.example.smproject.src.main.getPostApi.GetPostListView
+import com.example.smproject.src.main.getPostApi.models.GetPostListResonse
+import com.example.smproject.src.main.posted.PostedView
 import com.example.smproject.src.main.posted.models.Info
+import com.example.smproject.src.main.posted.models.PostedResponse
+import org.w3c.dom.Text
 
-class RecyclerViewAdapter(val datalist : ArrayList<Info>)
+class RecyclerViewAdapter
     : RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>(){
+    var dataList = mutableListOf<Info>()
 
-    inner class MyViewHolder(private val binding:ItemListBinding):RecyclerView.ViewHolder(binding.root){
-        fun bind(postedResponse: Info){
-//            binding.postPostedImg.text= postedResponse.imageUrlList
+    inner class MyViewHolder(private val binding:ItemListBinding):RecyclerView.ViewHolder(binding.root) {
+
+
+        fun bind(postedResponse: Info) {
             binding.postHashTag.text = postedResponse.hashtag
             binding.postContent.text = postedResponse.content
+
         }
     }
-
 
     //만들어진 뷰홀더 없을 때 뷰홀더(레이아웃) 생성하는 함수
     override fun onCreateViewHolder(parent:ViewGroup,viewType:Int):MyViewHolder{
@@ -26,11 +40,13 @@ class RecyclerViewAdapter(val datalist : ArrayList<Info>)
         return MyViewHolder(binding)
     }
 
-    override fun getItemCount():Int=datalist.size
+    override fun getItemCount():Int=dataList.size
 
     //recyclerview가 viewholder를 가져와 데이터 연결할 때 호출
 //적절한 데이터를 가져와서 그 데이터를 사용하여 뷰홀더의 레이아웃 채움
     override fun onBindViewHolder(holder:MyViewHolder,position:Int){
-        holder.bind(datalist[position])
+        holder.bind(dataList[position])
+
+
     }
 }
--- app/src/main/java/com/example/smproject/src/main/info/InfoPostActivity.kt
+++ app/src/main/java/com/example/smproject/src/main/info/InfoPostActivity.kt
@@ -1,66 +1,81 @@
 package com.example.smproject.src.main.info
 
-import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.os.bundleOf
-import androidx.fragment.app.setFragmentResult
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.smproject.databinding.ItemListBinding
 import com.example.smproject.databinding.PostListBinding
-import com.example.smproject.src.main.MainActivity
 import com.example.smproject.src.main.adapter.RecyclerViewAdapter
 import com.example.smproject.src.main.getPostApi.GetPostListService
 import com.example.smproject.src.main.getPostApi.GetPostListView
-import com.example.smproject.src.main.getPostApi.models.Data
 import com.example.smproject.src.main.getPostApi.models.GetPostListRequest
 import com.example.smproject.src.main.getPostApi.models.GetPostListResonse
-import com.example.smproject.src.main.posted.PostedRetrofitInterface
+import com.example.smproject.src.main.getPostApi.models.Post
 import com.example.smproject.src.main.posted.PostedService
 import com.example.smproject.src.main.posted.PostedView
 import com.example.smproject.src.main.posted.models.Info
 import com.example.smproject.src.main.posted.models.PostedRequest
 import com.example.smproject.src.main.posted.models.PostedResponse
-import com.example.smproject.src.main.search.SearchFragment
-import retrofit2.Call
-import retrofit2.Retrofit
 
 class InfoPostActivity:AppCompatActivity(), GetPostListView, PostedView{
     private lateinit var binding:PostListBinding
     private lateinit var adapter:RecyclerViewAdapter
-    val postLists = ArrayList<Info>()
-    // postLists : content, hashtag 담은 리스트
-    override fun onCreate(savedInstanceState: Bundle?){
-        super.onCreate(savedInstanceState)
 
-        binding=PostListBinding.inflate(layoutInflater)
+    var postLists = mutableListOf<Info>()
+    var getPostLists = mutableListOf<Post>()
+    var postAdd = mutableListOf<Info>()
+
+    // postLists : image, content, hashtag 담은 리스트
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = PostListBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initializelist()
+        GetPostListService(this).tryGetPostList(
+            GetPostListRequest(
+                "getPostList",
+                null,
+                null,
+                null,
+                1,
+                null
+            )
+        )
     }
-    private fun initializelist(){
-        val adapter=RecyclerViewAdapter(postLists)
 
-        binding.recyclerView.adapter=adapter
-        binding.recyclerView.layoutManager=LinearLayoutManager(this)
-        GetPostListService(this).tryGetPostList(GetPostListRequest("getPostList", null,null,null,1,null))
-
-    }
     override fun onGetPostListSuccess(response: GetPostListResonse) {
-        Log.d("게시물 목록", "")
+        getPostLists = response.data.list
+        for (i in 0 until getPostLists.size){
+            Log.d("게시물 id 목록 ", "${getPostLists[i].id}")
+            PostedService(this).tryGetPosted(PostedRequest("getPostInfo","${getPostLists[i].id}"))
+        }
+        Log.d("getPostList size ", "${getPostLists.size}")
+
 
     }
     override fun onGetPostListFailure(message: String) {
         Log.d("게시물 목록 요청 실패",message)
     }
+
     override fun onPostedSuccess(response: PostedResponse) {
-//        val postedId = response.data.info.id.toString()
-//        val postedContent = response.data.info.content
-//        val postedHashTag = response.data.info.hashtag
-        Log.d("게시물 정보 요청","성공")
+
+        postLists.add(response.data.info)
+
+        Log.d("adapter size", "${postLists.size}")
+
+        val adapter=RecyclerViewAdapter()
+        adapter.dataList=postLists
+        binding.recyclerView.adapter=adapter
+        binding.recyclerView.layoutManager=LinearLayoutManager(this)
     }
+
     override fun onPostedFailure(message: String) {
         Log.d("게시물 정보 요청 실패",message)
 
     }
+    fun addPost(){
+
+    }
 }
+
