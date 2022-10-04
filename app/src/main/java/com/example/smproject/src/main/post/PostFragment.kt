fe91cd84b52d2fcca6835fcd65fa8b21b9616979
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2022-10-04 22:22:02 +0900

    221004

--- app/src/main/java/com/example/smproject/src/main/post/PostFragment.kt
+++ app/src/main/java/com/example/smproject/src/main/post/PostFragment.kt
@@ -4,8 +4,7 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.database.Cursor
+import android.content.Intent.getIntent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Matrix
@@ -13,6 +12,7 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
+import android.os.Parcelable
 import android.provider.DocumentsContract
 import android.provider.MediaStore
 import android.text.Editable
@@ -26,9 +26,8 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.appcompat.widget.SwitchCompat
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
+import androidx.core.net.toUri
 import androidx.core.view.children
 import androidx.exifinterface.media.ExifInterface
 import androidx.fragment.app.Fragment
@@ -38,7 +37,6 @@ import com.amazonaws.mobileconnectors.s3.transferutility.*
 import com.amazonaws.regions.Region
 import com.amazonaws.regions.Regions
 import com.amazonaws.services.s3.AmazonS3Client
-import com.amazonaws.services.s3.internal.Constants
 import com.bumptech.glide.Glide
 import com.example.smproject.BuildConfig
 import com.example.smproject.R
@@ -50,12 +48,8 @@ import com.example.smproject.src.main.post.models.PostPostingResponse
 import com.example.smproject.util.BitmapConverter
 import com.example.smproject.util.CurrentLocation
 import com.google.android.material.chip.Chip
-import com.google.ar.core.TrackingState
 import java.io.File
 import java.io.IOException
-import java.util.jar.Manifest
-import javax.crypto.SecretKey
-import kotlin.collections.ArrayList
 
 
 class PostFragment : BaseFragment<FragmentPostBinding>(FragmentPostBinding::bind,R.layout.fragment_post),PostFragmentPostingView {
@@ -133,67 +127,19 @@ class PostFragment : BaseFragment<FragmentPostBinding>(FragmentPostBinding::bind
                     Glide.with(context)
                         .load(uri)
                         .into(binding.postPostedIv1)
-                    //Uri -> Bitmap -> Base64인코딩
-                    // 임시 주석 처리 (by 다인)
-//                    if (uri != null) {
-//                        // 사진 가져오기
-//                        val bitmap = BitmapFactory.decodeStream(context.contentResolver.openInputStream(uri))
-//                        // 사진의 회전 정보 가져오기
-//                        val orientation = getOrientationOfImage(uri).toFloat()
-//                        // 이미지 회전하기
-//                        val newBitmap = getRotatedBitmap(bitmap, orientation)
-////                        // 회전된 이미지로 imaView 설정
-////                        binding.postPostedIv1.setImageBitmap(newBitmap)
-//                        postImages.add("data:image/png;base64,${bitmapConverter.bitmapToBase64(newBitmap)}")
-//                    }
-
-                    // aws s3 image upload (by 다인)
                     if (uri != null) {
+                        // 사진 가져오기
+                        val bitmap = BitmapFactory.decodeStream(context.contentResolver.openInputStream(uri))
+                        // 사진의 회전 정보 가져오기
+                        val orientation = getOrientationOfImage(uri).toFloat()
+                        // 이미지 회전하기
+                        val newBitmap:Bitmap? = getRotatedBitmap(bitmap, orientation)
+//                        // 회전된 이미지로 imaView 설정
+                        binding.postPostedIv1.setImageBitmap(newBitmap)
+//                        postImages.add("data:image/png;base64,${bitmapConverter.bitmapToBase64(newBitmap)}")
+                        uriToFile(newBitmap?.let { bitmapConverter.getImageUri(context, it) } ,context) //업로드 및 list에 filename 추가
 
-                        // accesskey, secretkey는 깃허브에 올리면 안되서 따로 파일로 빼서 관리 부탁드립니다.
-                        val ACCESS_KEY = ""
-                        val SECRET_KEY = ""
-                        val awsCredentials: AWSCredentials = BasicAWSCredentials(ACCESS_KEY, SECRET_KEY);
-
-                        // aws s3 리전은 서울(AP_NORTHEAST_2)입니다.
-                        val s3Client: AmazonS3Client = AmazonS3Client(awsCredentials, Region.getRegion(Regions.AP_NORTHEAST_2))
-
-                        val transferUtility: TransferUtility = TransferUtility.builder().s3Client(s3Client).context(activity?.applicationContext).build()
-                        TransferNetworkLossHandler.getInstance(activity?.applicationContext)
-
-                        // getFilePathFromUri는 uri -> real path로 변환하는 함수로 아래 작성되어 있습니다.
-                        val path = getFilePathFromUri(context, uri)
-                        val file = File(path)
-
-                        // 버킷명: smwall
-                        // 파일이름: 파일이름의 경우 고유해야 해서 현재 시간을 TimeStamp로 변경한 뒤 확장자를 붙여서 사용합니다.
-                        val pathArr = path?.split(".")
-                        val type = pathArr?.get(pathArr?.size-1) // 확장자
-                        val filename = System.currentTimeMillis().toString() + "." + type
-                        val uploadObserver: TransferObserver = transferUtility.upload("smwall", filename, file) // 파일 업로드
-
-                        uploadObserver.setTransferListener(object : TransferListener {
-                            override fun onStateChanged(id: Int, state: TransferState?) {
-                                if(state == TransferState.COMPLETED) {
-                                    // 이미지 업로드 성공한 경우!!
-                                    // createPost하는 api의 imageList에 base64대신 위에서 만들어 놓은 filename을 넣어서 보내주시면 됩니다.
-                                    showCustomToast("UPLOAD_SUCCESS_STATE_CHANGED")
-                                }
-                            }
-                            override fun onProgressChanged(id: Int, bytesCurrent: Long, bytesTotal: Long) {
-                                showCustomToast("UPLOAD_SUCCESS_ON_PROGRESS_CHANGED")
-
-                            }
-
-
-                            override fun onError(id: Int, ex: java.lang.Exception?) {
-                                Log.d("error", ex.toString())
-                                showCustomToast("ERROR")
-
-                            }
-                        })
                     }
-                    //
                 }
                 else if(binding.postPostedIv1.drawable!=null){
                     if(binding.postPostedIv2.drawable!=null){
@@ -204,7 +150,7 @@ class PostFragment : BaseFragment<FragmentPostBinding>(FragmentPostBinding::bind
                         Glide.with(context)
                             .load(uri)
                             .into(binding.postPostedIv2)
-                        //Uri -> Bitmap -> Base64인코딩
+//                        Uri -> Bitmap -> Base64인코딩
                         if (uri != null) {
                             // 사진 가져오기
                             val bitmap = BitmapFactory.decodeStream(context.contentResolver.openInputStream(uri))
@@ -213,9 +159,14 @@ class PostFragment : BaseFragment<FragmentPostBinding>(FragmentPostBinding::bind
                             // 이미지 회전하기
                             val newBitmap = getRotatedBitmap(bitmap, orientation)
 //                            // 회전된 이미지로 imaView 설정
-//                            binding.postPostedIv2.setImageBitmap(newBitmap)
-                            postImages.add("data:image/png;base64,${bitmapConverter.bitmapToBase64(newBitmap)}")
+                            binding.postPostedIv2.setImageBitmap(newBitmap)
+//                            postImages.add("data:image/png;base64,${bitmapConverter.bitmapToBase64(newBitmap)}")
+                            uriToFile(newBitmap?.let { bitmapConverter.getImageUri(context, it) } ,context) //업로드 및 list에 filename 추가
+
+
                         }
+
+
                     }
                 }
             }
@@ -361,13 +312,13 @@ class PostFragment : BaseFragment<FragmentPostBinding>(FragmentPostBinding::bind
                                 it
                             )
                         }
+                        Log.d("카메라URI-show","${photoUri}")
                         intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
                         startActivityForResult(intent, REQ_CAMERA)
                     }
                 }
             }
         }
-
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -375,10 +326,21 @@ class PostFragment : BaseFragment<FragmentPostBinding>(FragmentPostBinding::bind
         if (resultCode == Activity.RESULT_OK) {
             when (requestCode) {
                 REQ_CAMERA -> {
+
+
                     val options:BitmapFactory.Options = BitmapFactory.Options()
                     options.inSampleSize = 2 //이미지 축소 정도. 원 크기에서 1/inSampleSize로 축소됨
                     bitmap = BitmapFactory.decodeFile(cameraImagePath,options)
-                    postImages.add("data:image/png;base64,${bitmapConverter.bitmapToBase64(bitmap)}")
+
+
+                    // 사진의 회전 정보 가져오기
+                    val orientation = getOrientationOfImage(bitmapConverter.getImageUri(context,bitmap)).toFloat()
+                    Log.d("회전각도", orientation.toString())
+                    // 이미지 회전하기
+                    val newBitmap = getRotatedBitmap(bitmap, orientation)
+
+                    uriToFile(newBitmap?.let { bitmapConverter.getImageUri(context, it) },context!!)
+//                    postImages.add("data:image/png;base64,${bitmapConverter.bitmapToBase64(bitmap)}")
 
                     if(binding.postPostedIv1.drawable==null){
                         binding.postPostedIv1.setImageBitmap(bitmap)
@@ -419,9 +381,7 @@ class PostFragment : BaseFragment<FragmentPostBinding>(FragmentPostBinding::bind
                 binding.postPostedCancel1.visibility = View.VISIBLE
                 binding.postPostedCancel2.visibility = View.GONE
             }
-            for(i in postImages){
-                showCustomToast("포스팅 이미지 ${i}")
-            }
+
         }
         binding.postPostedCancel2.setOnClickListener {
             if(postImages.size==2){
@@ -430,9 +390,7 @@ class PostFragment : BaseFragment<FragmentPostBinding>(FragmentPostBinding::bind
                 binding.postPostedCancel1.visibility = View.VISIBLE
                 binding.postPostedCancel2.visibility = View.GONE
             }
-            for(i in postImages){
-                showCustomToast("포스팅 이미지 ${i}")
-            }
+
         }
     }
 
@@ -466,8 +424,10 @@ class PostFragment : BaseFragment<FragmentPostBinding>(FragmentPostBinding::bind
     private fun getRotatedBitmap(bitmap: Bitmap?, degrees: Float): Bitmap? {
         if (bitmap == null) return null
         if (degrees == 0F) return bitmap
+
         val m = Matrix()
-        m.setRotate(degrees, bitmap.width.toFloat() / 2, bitmap.height.toFloat() / 2)
+        m.setRotate(degrees,bitmap.width.toFloat() / 2, bitmap.height.toFloat() / 2)
+//        m.setRotate(degrees, bitmap.width.toFloat() / 2, bitmap.height.toFloat() / 2)
         return Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, m, true)
     }
 
@@ -529,7 +489,10 @@ class PostFragment : BaseFragment<FragmentPostBinding>(FragmentPostBinding::bind
         Log.d("Post - 현재 위치","${ApplicationClass.latitude},${ApplicationClass.longtidute}")
 
         //error code 301 - 이미지와 내용 둘다 없는 경우
-//        Log.d("Posted","이미지 추가 완료 ${postImages[0]}, ${postImages[1]}")
+        if(postImages.size ==2){
+            Log.d("s3테스트","postImages -  ${postImages[0]},${postImages[1]}")
+        }
+
 
         PostPostingService(this).tryPostPosting(PostPostingRequest("createPost", contents, postImages, hashStringToPost, isPrivate, ApplicationClass.latitude.toString(),ApplicationClass.longtidute.toString()))
         binding.postPostedCancel1.visibility = View.GONE
@@ -566,5 +529,55 @@ class PostFragment : BaseFragment<FragmentPostBinding>(FragmentPostBinding::bind
     override fun onPostPostingFailure(message: String) {
         Log.d("포스팅 성공여부 ","실패 $message")
         dismissLoadingDialog()
+    }
+
+
+    //uri -> realpath -> filename  -> postImages.add(filename)
+    private fun uriToFile(uri:Uri?,context: Context){
+        // aws s3 image upload (by 다인)
+        if (uri != null) {
+
+            // accesskey, secretkey는 깃허브에 올리면 안되서 따로 파일로 빼서 관리 부탁드립니다. -> buildConfig에서 관리(gitignore에서 local property숨김)
+            val awsCredentials: AWSCredentials = BasicAWSCredentials(BuildConfig.S3_ACCESS_KEY,BuildConfig.S3_SECRET_KEY);
+
+            // aws s3 리전은 서울(AP_NORTHEAST_2)입니다.
+            val s3Client: AmazonS3Client = AmazonS3Client(awsCredentials, Region.getRegion(Regions.AP_NORTHEAST_2))
+
+            val transferUtility: TransferUtility = TransferUtility.builder().s3Client(s3Client).context(activity?.applicationContext).build()
+            TransferNetworkLossHandler.getInstance(activity?.applicationContext)
+
+            // getFilePathFromUri는 uri -> real path로 변환하는 함수로 아래 작성되어 있습니다.
+            val path = getFilePathFromUri(context, uri)
+            val file = File(path)
+
+            // 버킷명: smwall
+            // 파일이름: 파일이름의 경우 고유해야 해서 현재 시간을 TimeStamp로 변경한 뒤 확장자를 붙여서 사용합니다.
+            val pathArr = path?.split(".")
+            val type = pathArr?.get(pathArr?.size-1) // 확장자
+            val filename = System.currentTimeMillis().toString() + "." + type
+            val uploadObserver: TransferObserver = transferUtility.upload("smwall", filename, file) // 파일 업로드
+
+            uploadObserver.setTransferListener(object : TransferListener {
+                override fun onStateChanged(id: Int, state: TransferState?) {
+                    if(state == TransferState.COMPLETED) {
+                        // 이미지 업로드 성공한 경우!!
+                        // createPost하는 api의 imageList에 base64대신 위에서 만들어 놓은 filename을 넣어서 보내주시면 됩니다.
+                        postImages.add(filename)
+//                        showCustomToast("UPLOAD_SUCCESS_STATE_CHANGED")
+                    }
+                }
+                override fun onProgressChanged(id: Int, bytesCurrent: Long, bytesTotal: Long) {
+//                    showCustomToast("UPLOAD_SUCCESS_ON_PROGRESS_CHANGED")
+
+                }
+
+
+                override fun onError(id: Int, ex: java.lang.Exception?) {
+                    Log.d("error", ex.toString())
+//                    showCustomToast("ERROR")
+
+                }
+            })
+        }
     }
 }
